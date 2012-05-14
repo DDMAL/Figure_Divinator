77f52837ba14181d4a17427123996160796402f3
hannahilea <hannah.ilea.robertson@gmail.com>
2012-05-14 17:12:55 -0400

    Refactoring: import, parse, and output scores.

--- Hh_test.py
+++ Hh_test.py
@@ -1,60 +1,57 @@
 from music21 import *
 import copy
 
-score = converter.parse('xml_test_files_SL/Hotteterre-Op2-No3-1-Allemande.xml')
+url='xml_test_files_SL/Hotteterre-Op2-No3-1-Allemande.xml'
+
+#Import original
+score = converter.parse(url)
+oldtitle=""
+oldcomposer=""
+try:
+	oldtitle = score.metadata.title
+	oldcomposer= score.metadata.composer
+except:
+	oldtitle = str(url)
+
+#Make smaller for ease
 score = (score.measures(1, 4))
 
+#Get the bassline from the original
 bassnum = len(score.getElementsByClass(stream.Part)) - 1
 bassline = copy.deepcopy(score.getElementsByClass(stream.Part)[ bassnum ])
-
-chords = score.chordify()
-
-# for c in extraction.flat.getElementsByClass('Chord'):
-#     c.closedPosition(forceOctave=4, inPlace=True)
-#     c.removeRedundantPitches(inPlace=True)
-#     c.annotateIntervals()
-
 for n in bassline.flat:
     n.color = "red"
 
-for n in chords.flat:
-	n.color = "blue"
+#Get the chords from the original
+chords = score.chordify()
+for c in chords.flat.getElementsByClass('Chord'):
+    c.closedPosition(forceOctave=4, inPlace=True)
+    c.removeRedundantPitches(inPlace=True)
+    c.annotateIntervals()
+    c.color = "blue"
 
-score.show()
-bassline.show()
-chords.show()
+#Make the score to return (incl. fig. bass)
+reduction = stream.Score()
+reduction.insert(0,bassline)
 
-#bassline.insert(-1, chords.getElementsByClass(stream.Part)[0])
-#bassline.show()
-
-print "\nScore:"
-for x in score:
-	print x
-
-print "\nChords:"
-for x in chords:
-	print x
-
-print "\nBassline:"
-for x in bassline:
-	print x
+#Add metadata
+if not reduction.metadata:
+	reduction.insert(metadata.Metadata())
+reduction.metadata.title = 'Figured bass reduction of \n' + oldtitle + '\n\n'
+reduction.metadata.composer = oldcomposer + '\nFigure extraction: Automated'
 
 
-# score.insert(-1,extraction)
-# score.show()
+#Insert the solution below the reduction
+solution = tinyNotation.TinyNotationStream('E2_#6 F2_6')
+for n in solution.flat:
+	n.color="green"
+reduction.insert(0,solution)
 
+#Insert the original at the top
+for partline in score.getElementsByClass(stream.Part):
+ 	reduction.insert(-1,partline)
 
-# solution = tinyNotation.TinyNotationStream('E2_#6 F2_6')
+#Insert the chords above the reduction
+reduction.insert(-1,chords)
 
-# extraction.insert(0,solution)
-
-# for n in solution.flat:
-#     n.color = "blue"
-
-# for p in score.getElementsByClass(stream.Part):
-# 	extraction.insert(-1,p)
-
-
-# #extraction.insert(0,solution)
-# extraction.show()
-# score.show()
+reduction.show()
--- figure_extractor.py
+++ figure_extractor.py
@@ -7,10 +7,11 @@ import argparse
 import logging
 import copy
 
-from music21 import corpus
 from music21 import converter
-from music21 import tinyNotation
+from music21 import corpus
+from music21 import metadata
 from music21 import stream
+from music21 import tinyNotation
 
 #"rules" contains both the basic rules class and the additional rules sets
 import rules
@@ -22,21 +23,61 @@ LOG=Logging.getLogger('f_extractor')
 class FileNotFoundError(Exception):
     pass
 
-# class InputError(Exception):
-#     pass
+class InputError(Exception):
+    pass
 
 
 #When given a score, extract and return the figured bass
-def extract(score, ruleset=["SL"]):
-    extract = copy.deepcopy(score)
-    extract = extract.chordify()
+def extract(original_score, extraction_score, ruleset=["SL"]):
 
-    #TODO -- pull bassline
+    extract = copy.deepcopy(original_score)
 
-    #TODO -- pull chords
+    #Get the bassline from the original
+    bassline = get_bassline(original_score)
+
+    #Process the bassline
+    proc_bassline(bassline)
+
+    for n in bassline.flat:
+        n.color = "red"
+
+    #Get the chords from the original
+    chords = original_score.chordify()
+    for c in chords.flat.getElementsByClass('Chord'):
+        c.closedPosition(forceOctave=4, inPlace=True)
+        c.removeRedundantPitches(inPlace=True)
+        c.annotateIntervals()
+        c.color = "yellow"
+
     #TODO -- run rules
 
-    return extract
+    #Put bassline in extraction score
+    extraction_score.insert(0,bassline)
+
+    return extraction_score
+
+
+def get_bassline(score, proc_type="all"):
+    
+    try:
+        return copy.deepcopy(score['bass'])
+        LOG.debug("Bassline is pre-labeled bass line")
+
+    except:
+        try:
+            #Get all of lowest staff
+            bassnum = len(score.getElementsByClass(stream.Part)) - 1
+            return copy.deepcopy(score.getElementsByClass(stream.Part)[ bassnum ])
+            LOG.debug("Bassline is lowest part")
+        except:
+            raise InputError("cannot extract bass line from score")
+
+
+def proc_bassline(line, proc_type="all"):
+    #Post-processing:
+    #TODO - keep all notes?
+    #TODO - check to make sure aren't multiple bass notes
+    pass
 
 
 def create_comparison(extraction, original=False, solution="x" ):
@@ -47,16 +88,17 @@ def create_comparison(extraction, original=False, solution="x" ):
     #append the original to the extraction?
     if original != False:
         LOG.debug("appending original!")
-        for i  in range(0,len(original.parts)):
-            extraction.insert(-1, original.parts[i])
+        for partline in original.getElementsByClass(stream.Part):
+            extraction.insert(-1,partline)
 
     #append the solution to the extraction?
     if solution != "x":
         LOG.debug("appending solution!")
-        solution_stream = tinyNotation.TinyNotationStream(solution)
-        for n in solution_stream.flat.notes:
-            n.color = "red"
-        extraction.insert(0, solution_stream)
+        #Insert the solution below the reduction
+        solutionline = tinyNotation.TinyNotationStream(solution) #'E2_#6 F2_6')
+        for n in solutionline.flat:
+            n.color="red"
+        extraction.insert(0,solutionline)
 
     return extraction
 
@@ -79,10 +121,8 @@ def open_score_in_music21(scoreFile, ruleset=[""]):
     (input_base,ext_sep,input_ext) = scoreFile.rpartition(os.extsep)
 
     input_file_name = input_base + ext_sep + input_ext
-
     output_file_name = (input_base + "_figured_bass" + ext_rule +
                         ext_sep + input_ext)
-    print output_file_name
 
     # Open score with Music21
     try:
@@ -96,18 +136,39 @@ def open_score_in_music21(scoreFile, ruleset=[""]):
 def full_extraction(scorefile, ruleset='dummyrules', teststring = "x", display=True):
 
     #Read in score
-    original_score,output_file_name = open_score_in_music21(scorefile)
+    original_score,output_file_name = open_score_in_music21(scorefile, ruleset)
+
+    #Create new score to hold extraction
+    reduction = stream.Score()
+
+    #Add metadata
+    oldtitle = ""
+    oldcomposer = ""
+
+    try:
+        oldtitle = score.metadata.title
+    except:
+        oldtitle = str(scorefile)
+
+    try:
+        oldcomposer= score.metadata.composer
+    except:
+        pass
+
+    reduction.insert(metadata.Metadata())
+    reduction.metadata.title = 'Figured bass reduction of \n' + oldtitle
+    reduction.metadata.composer = oldcomposer + '\nFigure extraction: Auto'
 
     #extract figured bass
-    extraction = extract(original_score, ruleset)
+    reduction = extract(original_score, reduction, ruleset)
 
     #append original score and/or solution to test file
     if teststring != False:
-        create_comparison(extraction, original_score, teststring)
+        create_comparison(reduction, original_score, teststring)
 
     #save the file
     LOG.debug('\tSaving the file!')
-    extraction.write(fmt='musicxml', fp=output_file_name)
+    reduction.write(fmt='musicxml', fp=output_file_name)
 
     #display the new file?
     if teststring != False or display == True:
@@ -119,14 +180,8 @@ def full_extraction(scorefile, ruleset='dummyrules', teststring = "x", display=T
             LOG.info("Unable to show .xml output through MusicXML,")
             LOG.info("try opening the file directly.")
 
-
-
-
-
-
     #done!
     LOG.info('Done with full extraction.')
-
 
 
 # Run from command line
@@ -151,5 +206,5 @@ if __name__ == "__main__":
     testString = args.test_string
     viewOutput = args.viewOutput
 
-    full_extraction(scoreFile, ruleSet, testString, viewOutput, True)
+    full_extraction(scoreFile, ruleSet, testString, True)
     LOG.info("cl done")
