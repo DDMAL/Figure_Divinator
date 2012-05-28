8dd9f660b16b3885e56d7e71f9bb4205de701540
hannahilea <hannah.ilea.robertson@gmail.com>
2012-05-28 13:17:53 -0400

    Combined score crawler with rules class, deleted overlapping files.

--- rules.py
+++ rules.py
@@ -2,7 +2,7 @@
 # Implements basic rule class; imports specialized rule classes
 # depending on what rule set was called; tests the list of rules
 
-from music21 import interval
+#from music21 import interval
 
 import logging_setup as Logging
 LOG=Logging.getLogger('rules')
@@ -17,6 +17,22 @@ class Rule(object):
 
     def apply(self,context):
         pass
+
+#* * * * Interval key:
+# unison:            interval.ChromaticInterval(0)
+# semitone up:       interval.ChromaticInterval(1)
+# semitone down:     interval.ChromaticInterval(-1)
+# tone up:           interval.ChromaticInterval(2)
+# tone down:         interval.ChromaticInterval(-2)
+# minor third up:    interval.ChromaticInterval(3)
+# minor third down:  interval.ChromaticInterval(-3)
+# major third up:    interval.ChromaticInterval(4)
+# major third down:  interval.ChromaticInterval(-4)
+# perfect fourth:    interval.ChromaticInterval(5)
+# diminished fifth:  interval.ChromaticInterval(6)
+# perfect fifth:     interval.ChromaticInterval(7)
+# minor sixth:       interval.ChromaticInterval(8)
+# major sixth:       interval.ChromaticInterval(9)
 
 
 from ruleset_octave import *
@@ -84,3 +100,54 @@ def get_rules(ruleset):
         raise RuleImplementationError()
 
     return extraction_rules
+
+class rule_crawler(object):
+    def __init__(self, score, ruleset):
+        self.score = score
+        self.ruleset = []
+        self.direction = kwargs.get('direction','backward')
+
+        self.total_length
+        self.rule_max
+        self.rule_min
+
+        self._load_score()
+        self._load_rules(ruleset)
+
+    def _load_score(self):
+        self.total_length = len(score._fbline_stream.flat.getElementsByClass(note.Note))
+        self.rule_min = self.total_length
+
+    def _load_rules(self):
+        self.ruleset = rules.getRules(self.ruleset) #TODO
+        for rule in self.ruleset:
+            if rule.size > self.rule_max: self.rule_max = rule.size
+            if rule.size < self.rule_min: self.rule_min = rule.size
+
+    def _chunkify(self,start_index,end_index):
+        chunk.intervals = []
+        chunk.content = []
+        chunk.extra = []
+        chunk.
+        return chunk
+
+    def apply_rules(self,where='notes'):
+        applying = True
+
+        while applying == True:
+
+            #get chunk
+            START = 1 #TODO
+            END = 5 #TODO
+            chunk = self.score._chunkify(START,END)
+            if True: applying = False #todo update
+
+            #for each rule in rules, try rule
+            for rule in self.ruleset:
+                figures = rule.test_rule(chunk)
+                LOG.info("%s is %s at chunk for range %s - %s", rule.__class__.__name__, str(figures), str(START), str(END))
+                if figures:
+                    #write to notes? #write to log?
+                    #For now, notes (make optional)
+                    for i in range(len(figures)):
+                        score_note_[i].addlyricssomehow(figures[i])
