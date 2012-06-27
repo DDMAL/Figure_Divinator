87aca06f51bbe7d444e82975100397b12d1d6e9b
hannahilea <hannah.ilea.robertson@gmail.com>
2012-06-27 18:17:14 -0400

    Automated ability to find, add all rules at once. Created a ruleset class.

--- rules.py
+++ rules.py
@@ -1,5 +1,6 @@
 # Figured bass extractor rules:
 # basic rule class; tests the list of rules
+import os
 
 
 class Rule(object):
@@ -16,3 +17,36 @@ class Rule(object):
     def show_it_off(self):
         from pprint import pprint
         pprint(vars(self))
+
+
+class Ruleset(object):
+    def __init__(self, rules, **kwargs):
+        self.rulelist = []
+        self.name = kwargs.get('name', 'Unique Rule Set')
+        self.metadata = kwargs.get('metadata', {})
+        load = kwargs.get('from_module', False)
+
+        if load:
+            self.rulelist = rules
+
+        else:
+            for rule in rules:
+                #Try adding it from string
+                try:
+                    new_rule = globals()[rule]()
+                    self.rulelist.append(new_rule)
+                except Exception:
+                    print rule + ' doesn\'t register as a rule...'
+
+    def check_validity(self):
+        pass  # TODO: make sure all rules are valid
+
+
+#Import all the rulesets in the 'rulesets' directory:
+fullRulesets = {}
+for filename in os.listdir('rulesets'):
+    if (filename == 'ruleset_template.py' or
+        filename[-3:] != '.py' or filename == '__init__.py'):
+        continue
+    filename = 'rulesets.' + filename
+    module = __import__(filename[:-3], locals(), globals())
