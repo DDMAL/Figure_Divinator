f49690c208f1052cc20a79557523a4c419f42a5d
hannahilea <hannah.ilea.robertson@gmail.com>
2012-06-28 14:22:44 -0400

    Template solution file for Konstantin.

---
+++ testfiles_SL_solutions.py
@@ -0,0 +1,5 @@
+SL_solutions = {
+ #  'RULE_NAME_.xml'    : "SOLUTION STRING",            # Page number: #
+    'SLRule_10b_1.xml'  : "C2 B2_6",                    # Page number: 47
+    'SLRule_10b_2.xml'  : "C2_5 B2_2"                   # Page number: 48
+}
