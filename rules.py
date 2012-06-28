8a2743673502af54b3c5ba683baedf3e394dcf18
hannahilea <hannah.ilea.robertson@gmail.com>
2012-06-28 16:08:55 -0400

    Ensure that a packaged rule set can be found even if it is in a list of length 1.

--- rules.py
+++ rules.py
@@ -2,6 +2,9 @@
 # basic rule class; tests the list of rules
 import os
 
+packagedRulesets = {}
+full_rule_dictionary = {}
+
 
 class Rule(object):
     def __init__(self):
@@ -19,6 +22,24 @@ class Rule(object):
         pprint(vars(self))
 
 
+def get_ruleset(input_item):
+    """
+    Given the key to a packaged rule set or a list of rules,
+    return (or create and then return) a rule set.
+    """
+    try:
+        return packagedRulesets[input_item]
+
+    except:
+        #Perhaps key has been stored in an array rather than as just a string...
+        try:
+            return packagedRulesets[input_item[0]]
+
+        #If input is list of rules, rather than a key, make a new ruleset
+        except:
+            return Ruleset(input_item)
+
+
 class Ruleset(object):
     def __init__(self, rules, **kwargs):
         self.rulelist = []
@@ -26,28 +47,25 @@ class Ruleset(object):
         self.metadata = kwargs.get('metadata', {})
         load = kwargs.get('from_module', False)
 
+        # Load is true if this ruleset is being built from a module script
         if load:
             self.rulelist = rules
 
+        # Otherwise, ruleset is being built from individual rules
         else:
             for r in rules:
-                #Try adding it from string
                 try:
                     rule = full_rule_dictionary[r]
                     new_rule = rule()
-                    #new_rule = r()
                     self.rulelist.append(new_rule)
                 except Exception:
-                    raise
-                    print rule + " doesn't register as a rule..."
+                    print "Sorry, '" + r + "'' doesn't register as a rule..."
 
     def check_validity(self):
         pass  # TODO: make sure all rules are valid
 
 
 #Import all the rulesets in the 'rulesets' directory:
-packagedRulesets = {}
-full_rule_dictionary = {}
 for filename in os.listdir('rulesets'):
     if (filename == 'ruleset_template.py' or
         filename[-3:] != '.py' or filename == '__init__.py'):
