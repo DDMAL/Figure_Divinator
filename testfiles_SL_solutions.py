a1dd15023e316435cacfae2b9f8d23c2802fcb81
konstantin8806 <kosta_bozhinov@hotmail.com>
2012-06-28 17:11:06 -0700

    Update master

--- testfiles_SL_solutions.py
+++ testfiles_SL_solutions.py
@@ -2,4 +2,4 @@ SL_solutions = {
  #  'RULE_NAME_.xml'    : "SOLUTION STRING",            # Page number: #
     'SLRule_10b_1.xml'  : "C2 B2_6",                    # Page number: 47
     'SLRule_10b_2.xml'  : "C2_5 B2_2"                   # Page number: 48
-}
+}SLRule_3_1.xml : A2_6 Bb2
