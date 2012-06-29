5059371b6e8eaf755d4782ef7b22443897e5933d
konstantin8806 <kosta_bozhinov@hotmail.com>
2012-06-28 17:34:08 -0700

    Update master

--- testfiles_SL_solutions.py
+++ testfiles_SL_solutions.py
@@ -2,4 +2,21 @@ SL_solutions = {
  #  'RULE_NAME_.xml'    : "SOLUTION STRING",            # Page number: #
     'SLRule_10b_1.xml'  : "C2 B2_6",                    # Page number: 47
     'SLRule_10b_2.xml'  : "C2_5 B2_2"                   # Page number: 48
-}SLRule_3_1.xml : A2_6 Bb2
+}   'SLRule_3_1.xml'    : "A2_6 Bb2"                    # Page number: 45
+    'SLRule_3_2.xml'    : "D2_6 Eb2"                    # Page number: 45
+    'SLRule_3_3.xml'    : "G2_6 Ab2"                    # Page number: 45
+    'SLRule_4_1.xml'    : "A2 Bb2_6"                    # Page number: 46
+    'SLRule_4_2.xml'    : "A2 Bb2_6"                    # Page number: 46
+    'SLRule_5_1'        : "F2_6 E2"                     # Page number: 46
+    'SLRule_5_2'        : "Bb2_6 A2"                    # Page number: 46
+    'SLRule_6_1'        : "E2_#6 F2_6"                  # Page number: 46
+    'SLRule_6_2'        : "B2_#6 C2_6"                  # Page number: 46
+    'SLRule_6_3'        : "E2_#6 F2_6"                  # Page number: 46
+    'SLRule_6_4'        : "B2_#6 C2_6"                  # Page number: 46
+    'SLRule_7_1'        : "D2 B2 C2_b A2 Bb1"           # Page number: 47
+    'SLRule_7_2'        : "G2_b E2 F2_b Eb1"            # Page number: 47
+    'SLRule_8b_1'       : "F2 D2_6,b Eb2 C2_6 D1_#"     # Page number: 47
+    'SLRule_8b_2'       : "F4 Eb4 D2_6,b Eb4 D4 C2_6 D1_#" # Page number: 47
+    'SLRule_8a_1'       : "F2 D2 Eb2 C2 D1_#"           # Page number: 47
+    'SLRule_8a_2'       : "F4 Eb4 D2 Eb4 D4 C2 D1_#"    # Page number: 47
+    'SLRule_10_a'       : "A4 F#4_6 D4 E4_# C#4_6 A4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 B8 A4_# E4_# E4_7 A2+4_#" # Page number: 47
