2bb97517cfaff5f40a8f3f157a954c0a52d68288
hannahilea <hannah.ilea.robertson@gmail.com>
2012-07-03 15:31:48 -0400

    Show rule comparison through rulesViewer also.

--- rulesViewer.py
+++ rulesViewer.py
@@ -39,16 +39,30 @@ parser.add_argument('-content', action='store_true',
 parser.add_argument('-extras', action='store_true',
                     dest='viewExtra', default=False,
                     help='Just extras')
+parser.add_argument('-compare', action='store_true',
+                    dest='compare', default=False,
+                    help='Show rule comparisons?')
 parser.add_argument('-r', nargs='*', dest='rules_type', default='SL', help='Set of rules to list')
 
 #Set flags
 args = parser.parse_args()
 ruleSet = args.rules_type
 
+toView = False
+toCompare = args.compare
+
+if (args.viewTodo == True or args.viewSize == True or
+    args.viewIntervals == True or args.viewBeats == True or
+    args.viewFigures == True or args.viewContent == True or
+    args.viewExtra == True):
+    toView = True
+
+
 if (args.viewTodo == False and args.viewSize == False and
     args.viewIntervals == False and args.viewBeats == False and
     args.viewFigures == False and args.viewContent == False and
-    args.viewExtra == False):
+    args.viewExtra == False and args.compare == False):
+        toView = True
         args.viewTodo = True
         args.viewSize = True
         args.viewIntervals = True
@@ -61,76 +75,83 @@ if (args.viewTodo == False and args.viewSize == False and
 # Get the extraction rules
 extraction_rules = rules.get_ruleset(ruleSet).rulelist
 
-rules_umbrella = ""
+#Are we viewing the rules?
+if toView:
+    #Run through and list the various qualities of the rules
+    rules_umbrella = ""
+    for rule in extraction_rules:
+        if rules_umbrella != rule.umbrella:
+            rules_umbrella = rule.umbrella
+            LOG.info("* * RULE SET: %s * *", rules_umbrella)
 
-for rule in extraction_rules:
-    if rules_umbrella != rule.umbrella:
-        rules_umbrella = rule.umbrella
-        LOG.info("* * RULE SET: %s * *", rules_umbrella)
+        named = False
 
-    named = False
+        def not_all_false(items):
+            return not all(x == False for x in items)
 
-    def not_all_false(items):
-        return not all(x == False for x in items)
+        if args.viewSize == True:
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            LOG.info("          size: " + str(rule.size))
 
-    if args.viewSize == True:
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        LOG.info("          size: " + str(rule.size))
+        if args.viewIntervals == True and not_all_false(rule.intervals):
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            intstr = ''
+            for i in range(len(rule.intervals)):
+                intstr = intstr + '{'
+                for j in range(len(rule.intervals[i])):
+                    if j > 0:
+                        intstr = intstr + ' or '
+                    intstr = intstr + str(rule.intervals[i][j].simpleDirected)
+                intstr = intstr + '} '
+            LOG.info("     intervals: " + intstr)
 
-    if args.viewIntervals == True and not_all_false(rule.intervals):
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        intstr = ''
-        for i in range(len(rule.intervals)):
-            intstr = intstr + '{'
-            for j in range(len(rule.intervals[i])):
-                if j > 0:
-                    intstr = intstr + ' or '
-                intstr = intstr + str(rule.intervals[i][j].simpleDirected)
-            intstr = intstr + '} '
-        LOG.info("     intervals: " + intstr)
+        if args.viewBeats == True and not_all_false(rule.beats):
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            LOG.info("         beats: " + str(rule.beats))
 
-    if args.viewBeats == True and not_all_false(rule.beats):
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        LOG.info("         beats: " + str(rule.beats))
+        if args.viewContent == True and not_all_false(rule.harmonic_content):
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            LOG.info("      hcontent: " + str(rule.harmonic_content))
 
-    if args.viewContent == True and not_all_false(rule.harmonic_content):
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        LOG.info("      hcontent: " + str(rule.harmonic_content))
+        if args.viewExtra == True and not_all_false(rule.extras):
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            LOG.info("         extra: " + str(rule.extras))
 
-    if args.viewExtra == True and not_all_false(rule.extras):
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        LOG.info("         extra: " + str(rule.extras))
+        if args.viewFigures == True and not_all_false(rule.figures):
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            try:
+                figstr = '[' + rule.figures[0].notationColumn + ']'
+            except:  # TODO - figure out type of exception
+                figstr = '[NA]'
+                for i in range(1, len(rule.figures)):
+                    try:
+                        figstr = figstr + '; [' + rule.figures[i].notationColumn + ']'
+                    except:  # TODO - figure out type of exception
+                        figstr = figstr + '; [NA]'
 
-    if args.viewFigures == True and not_all_false(rule.figures):
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        try:
-            figstr = '[' + rule.figures[0].notationColumn + ']'
-        except:  # TODO - figure out type of exception
-            figstr = '[NA]'
-            for i in range(1, len(rule.figures)):
-                try:
-                    figstr = figstr + '; [' + rule.figures[i].notationColumn + ']'
-                except:  # TODO - figure out type of exception
-                    figstr = figstr + '; [NA]'
+            LOG.info("       figures: " + figstr)
 
-        LOG.info("       figures: " + figstr)
+        if args.viewTodo == True and rule.todo != "undefined":
+            if named == False:
+                LOG.info("\n" + rule.__class__.__name__ + ":")
+                named = True
+            LOG.info("         to do: %s", rule.todo)
 
-    if args.viewTodo == True and rule.todo != "undefined":
-        if named == False:
-            LOG.info("\n" + rule.__class__.__name__ + ":")
-            named = True
-        LOG.info("         to do: %s", rule.todo)
+    LOG.info("* * DONE REVIEWING RULES. * *")
 
-LOG.info("* * DONE REVIEWING RULES. * *")
+#Are we comparing the rules?
+if toCompare:
+    rules.compare_rules_in_list(extraction_rules)
+    LOG.info("* * DONE COMPARING RULES. * *")
