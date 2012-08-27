ede9c15b675e76d0db7fd377f7bbef42851adfeb
hannahilea <hannah.ilea.robertson@gmail.com>
2012-08-27 10:09:49 -0400

    Added correct comma, '.xml' formatting to testfiles_SL_solutions.

--- testfiles_SL_solutions.py
+++ testfiles_SL_solutions.py
@@ -1,49 +1,50 @@
 SL_solutions = {
- #  'RULE_NAME_.xml'    : "SOLUTION STRING",            # Page number: #
-    'SLRule_3_1.xml'    : "A2_6 Bb2",                   # Page number: 45
-    'SLRule_3_2.xml'    : "D2_6 Eb2",                   # Page number: 45
-    'SLRule_3_3.xml'    : "G2_6 Ab2",                   # Page number: 45
-    'SLRule_4_1.xml'    : "A2 Bb2_6",                   # Page number: 46
-    'SLRule_4_2.xml'    : "A2 Bb2_6",                   # Page number: 46
-    'SLRule_5_1'        : "F2_6 E2",                    # Page number: 46
-    'SLRule_5_2'        : "Bb2_6 A2",                   # Page number: 46
-    'SLRule_6_1'        : "E2_#6 F2_6",                 # Page number: 46
-    'SLRule_6_2'        : "B2_#6 C2_6",                 # Page number: 46
-    'SLRule_6_3'        : "E2_#6 F2_6",                 # Page number: 46
-    'SLRule_6_4'        : "B2_#6 C2_6",                 # Page number: 46
-    'SLRule_7_1'        : "D2 B2 C2_b A2 Bb1",          # Page number: 47
-    'SLRule_7_2'        : "G2_b E2 F2_b Eb1",           # Page number: 47
-    'SLRule_8b_1'       : "F2 D2_6,b Eb2 C2_6 D1_#",    # Page number: 47
-    'SLRule_8b_2'       : "F4 Eb4 D2_6,b Eb4 D4 C2_6 D1_#",  # Page number: 47
-    'SLRule_8a_1'       : "F2 D2 Eb2 C2 D1_#",          # Page number: 47
-    'SLRule_8a_2'       : "F4 Eb4 D2 Eb4 D4 C2 D1_#",   # Page number: 47
-    'SLRule_10_a'       : "A4 F#4_6 D4 E4_# C#4_6 A4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 B8 A4_# E4_# E4_7 A2+4_#"  # Page number: 47
-    'SLRule_11'         : "C2 A2_6"                     # Page number: 47
-    'SLRule_12'         : "F2 B2"                       # Page number: 48
-    'SLRule_13'         : "C2 E2_6"                     # Page number: 48
-    'SLRule_14'         : "E4_6 F4_65 G4"               # Page number: 48
-    'SLRule_15'         : "D4 C4_6#42 B2_6"             # Page number: 49
-    'SLRule_16'         : "D4 C4_6#42 Bb2_7"            # Page number: 49 
-    'SLRule_17'         : "D2 Bb2_6 C2"                 # Page number:
-    'SLRule_18'         : "G2 E2_6 F2"                  # Page number: 50
-    'SLRule_19'         : "B2_653 G2_7 C1"              # Page number: 51
-    'SLRule_20'         : "E2_6b5 F2 C2"                # Page number: 50
-    'SLRule_21'         : "C2 C2_6 G"                   # Page number: 51
-    'SLRule_22'         : "C2 C2_6#42 G2"               # Page number: 51
-    'SLRule_23'         : "C4 D4_6 E2_6b53 F1"          # Page number: 51
-    'SLRule_24a'        : "F4 E4_6 D4_#6 C2+4"          # Page number: 52
-    'SLRule_24b1'       : "F4 Eb4_6#42 D4_#6 C2+4_b"    # Page number: 52-53
-    'SLRule_24b2'       : "F4_b E4_642 D4_#643 C2+4_b"  # Page number: 52-53
-    'SLRule_24c'        : "A4 G4 F4_6 E2+4_#"           # Page number: 53
-    'SLRule_25a'        : "G4 F4_6#42 E4_6 D4_#6 C1"    # Page number: 54
-    'SLRule_25b'        : "G4 F4_6#42 Eb4_6 D4_#6 C1_b" # Page number: 54
-    'SLRule_26a1'       : "G4 F4_6#42 E4_6 D4_#6 C4 B1_6" # Page number: 54
-    'SLRule_26a2'       : "G4 F4_6#42 E4_6 D4_#6 C4 G1" # Page number: 54
-    'SLRule_27a'        : "C4 D4_643 E4_6 F4_653 G1"    # Page number: 55
-    'SLRule_27b'        : "C4_b D4_643 Eb4_6 F4_653 G1" # Page number: 55
-    'SLRule_28'         : "G2_54 G2_7 C1"               # Page number: 55
-    'SLRule_29'         : "G1_54,7 C1"                  # Page number: 55
-    'SLRule_30'         : "G4_7 C1"                     # Page number: 56
-    'SLRule_31a'        : "G2+4_54,7 C2+4"              # Page number: 56
-    'SLRule_31b'        : "G2+4_54,7 C2+4"              # Page number: 56
-    'SLRule_32'         : "G2+4_54,853,753 C2+4"        # Page number: 56
+ #  'RULE_NAME_.xml'    : "SOLUTION STRING",                # Page number: #
+    'SLRule_3_1.xml'    : "A2_6 Bb2",                       # Page number: 45
+    'SLRule_3_2.xml'    : "D2_6 Eb2",                       # Page number: 45
+    'SLRule_3_3.xml'    : "G2_6 Ab2",                       # Page number: 45
+    'SLRule_4_1.xml'    : "A2 Bb2_6",                       # Page number: 46
+    'SLRule_4_2.xml'    : "A2 Bb2_6",                       # Page number: 46
+    'SLRule_5_1.xml'    : "F2_6 E2",                        # Page number: 46
+    'SLRule_5_2.xml'    : "Bb2_6 A2",                       # Page number: 46
+    'SLRule_6_1.xml'    : "E2_#6 F2_6",                     # Page number: 46
+    'SLRule_6_2.xml'    : "B2_#6 C2_6",                     # Page number: 46
+    'SLRule_6_3.xml'    : "E2_#6 F2_6",                     # Page number: 46
+    'SLRule_6_4.xml'    : "B2_#6 C2_6",                     # Page number: 46
+    'SLRule_7_1.xml'    : "D2 B2 C2_b A2 Bb1",              # Page number: 47
+    'SLRule_7_2.xml'    : "G2_b E2 F2_b Eb1",               # Page number: 47
+    'SLRule_8b_1.xml'   : "F2 D2_6,b Eb2 C2_6 D1_#",        # Page number: 47
+    'SLRule_8b_2.xml'   : "F4 Eb4 D2_6,b Eb4 D4 C2_6 D1_#",  # Page number: 47
+    'SLRule_8a_1.xml'   : "F2 D2 Eb2 C2 D1_#",              # Page number: 47
+    'SLRule_8a_2.xml'   : "F4 Eb4 D2 Eb4 D4 C2 D1_#",       # Page number: 47
+    'SLRule_10_a.xml'   : "A4 F#4_6 D4 E4_# C#4_6 A4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 B8 A4_# E4_# E4_7 A2+4_#",  # Page number: 47
+    'SLRule_11.xml'     : "C2 A2_6",                        # Page number: 47
+    'SLRule_12.xml'     : "F2 B2",                          # Page number: 48
+    'SLRule_13.xml'     : "C2 E2_6",                        # Page number: 48
+    'SLRule_14.xml'     : "E4_6 F4_65 G4",                  # Page number: 48
+    'SLRule_15.xml'     : "D4 C4_6#42 B2_6",                # Page number: 49
+    'SLRule_16.xml'     : "D4 C4_6#42 Bb2_7",               # Page number: 49
+    'SLRule_17.xml'     : "D2 Bb2_6 C2",                    # Page number:
+    'SLRule_18.xml'     : "G2 E2_6 F2",                     # Page number: 50
+    'SLRule_19.xml'     : "B2_653 G2_7 C1",                 # Page number: 51
+    'SLRule_20.xml'     : "E2_6b5 F2 C2",                   # Page number: 50
+    'SLRule_21.xml'     : "C2 C2_6 G",                      # Page number: 51
+    'SLRule_22.xml'     : "C2 C2_6#42 G2",                  # Page number: 51
+    'SLRule_23.xml'     : "C4 D4_6 E2_6b53 F1",             # Page number: 51
+    'SLRule_24a.xml'    : "F4 E4_6 D4_#6 C2+4",             # Page number: 52
+    'SLRule_24b1.xml'   : "F4 Eb4_6#42 D4_#6 C2+4_b",       # Page number: 52-53
+    'SLRule_24b2.xml'   : "F4_b E4_642 D4_#643 C2+4_b",     # Page number: 52-53
+    'SLRule_24c.xml'    : "A4 G4 F4_6 E2+4_#",              # Page number: 53
+    'SLRule_25a.xml'    : "G4 F4_6#42 E4_6 D4_#6 C1",       # Page number: 54
+    'SLRule_25b.xml'    : "G4 F4_6#42 Eb4_6 D4_#6 C1_b",    # Page number: 54
+    'SLRule_26a1.xml'   : "G4 F4_6#42 E4_6 D4_#6 C4 B1_6",  # Page number: 54
+    'SLRule_26a2.xml'   : "G4 F4_6#42 E4_6 D4_#6 C4 G1",    # Page number: 54
+    'SLRule_27a.xml'    : "C4 D4_643 E4_6 F4_653 G1",       # Page number: 55
+    'SLRule_27b.xml'    : "C4_b D4_643 Eb4_6 F4_653 G1",    # Page number: 55
+    'SLRule_28.xml'     : "G2_54 G2_7 C1",                  # Page number: 55
+    'SLRule_29.xml'     : "G1_54,7 C1",                     # Page number: 55
+    'SLRule_30.xml'     : "G4_7 C1",                        # Page number: 56
+    'SLRule_31a.xml'    : "G2+4_54,7 C2+4",                 # Page number: 56
+    'SLRule_31b.xml'    : "G2+4_54,7 C2+4",                 # Page number: 56
+    'SLRule_32.xml'     : "G2+4_54,853,753 C2+4"            # Page number: 56
+    }
