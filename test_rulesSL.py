84800698409f2e84e58b835daf1d75929ca19a42
hannahilea <hannah.ilea.robertson@gmail.com>
2012-08-28 15:34:36 -0400

    Fixed tinynotation strings in test_rulesSL

--- test_rulesSL.py
+++ test_rulesSL.py
@@ -40,30 +40,29 @@ def _SL_full_test(chosen_rules=False, clean=True):
         '08b_2'  : "F4 E-4 D2_6,b E-4 D4 C2_6 D1_#",  # Page number: 47
         '08a_1'  : "F2 D2 E-2 C2 D1_#",              # Page number: 47
         '08a_2'  : "F4 E-4 D2 Eb4 D4 C2 D1_#",       # Page number: 47
-        '10a'    : "A4 F#4_6 D4 E4_# C#4_6 AA4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 BB8 AA4_# E4_# EE4_7 AA2_#",  # Page number: 47
-        #'10a'    : "A4 F#4_6 D4 E4_# C#4_6 AA4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 BB8 AA4_# E4_# EE4_7 AA2+4_#",  # Page number: 47
+        '10a'    : "A4 F#4_6 D4 E4_# C#4_6 AA4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 BB8 AA4_# E4_# EE4_7 AA2._#",  # Page number: 47
         #'10a'    : "3/4 A4 F#4_6 D4 E4_# C#4_6 AA4 D8 C#8 D8 E8 F#8 D8 E8_# F#8 E8 D8 C#8 BB8 AA4_# E4_# EE4_7 AA2+4_#",  # Page number: 47
-        '11'     : "r4 C2 AA2_6",                        # Page number: 47
+        '11'     : "C2 AA2_6",                        # Page number: 47
         '12'     : "F2 BB2",                          # Page number: 48
         '13'     : "c2 E2_6",                        # Page number: 48
         '14'     : "EE4_6 FF4_6,5 GG4",                  # Page number: 48
-        '15'     : "D4 C4_6,#4,2 BB2_6",                # Page number: 49
-        '16'     : "D4 C4_6,#4,2 BB-2_7",               # Page number: 49
+        '15'     : "D4 C4_6,4+,2 BB2_6",                # Page number: 49
+        '16'     : "D4 C4_6,4+,2 BB-2_7",               # Page number: 49
         '17'     : "D2 BB-2_6 C2",                    # Page number:
         '18'     : "r2 GG2 EE2_6 FF2",                     # Page number: 50
         '19'     : "r2 BB2_6,5,3 GG2_7 C1",                 # Page number: 51
-        '20'     : "E2_6-5 F2 C2",                   # Page number: 50
+        '20'     : "E2_6,5b F2 C2",                   # Page number: 50
         '21'     : "r2 C2 C2_6 GG",                      # Page number: 51
-        '22'     : "r2 C2 C2_6,#4,2 GG2",                  # Page number: 51
+        '22'     : "r2 C2 C2_6,4+,2 GG2",                  # Page number: 51
         '23'     : "r2 C4 D4_6 E2_6,b5,3 F1",             # Page number: 51
         '24a'    : "F4 E4_6 D4_6+ C2+4",             # Page number: 52
-        '24b1'   : "r4 F4 E-4_6,#4,2 D4_6+ C2+4_b",       # Page number: 52-53
+        '24b1'   : "r4 F4 E-4_6,4+,2 D4_6+ C2+4_b",       # Page number: 52-53
         '24b2'   : "r4 F4_b E-4_6,4,2 D4_6+,4,3 C2+4_b",     # Page number: 52-53
         '24c'    : "r4 A4 G4 F4_6 E2+4_#",              # Page number: 53
-        '25a'    : "r4 G4 F4_6,#4,2 E4_6 D4_6+ C1",       # Page number: 54
-        '25b'    : "G4 F4_6,#4,2 E-4_6 D4_6+ C1_b",    # Page number: 54
-        '26a1'   : "G4 F4_6,#4,2 E4_6 D4_6+ C4 BB1_6",  # Page number: 54
-        '26a2'   : "G4 F4_6,#4,2 E4_6 D4_6+ C4 GG1",    # Page number: 54
+        '25a'    : "r4 G4 F4_6,4+,2 E4_6 D4_6+ C1",       # Page number: 54
+        '25b'    : "G4 F4_6,4+,2 E-4_6 D4_6+ C1_b",    # Page number: 54
+        '26a1'   : "G4 F4_6,4+,2 E4_6 D4_6+ C4 BB1_6",  # Page number: 54
+        '26a2'   : "G4 F4_6,4+,2 E4_6 D4_6+ C4 GG1",    # Page number: 54
         '27a'    : "C4 D4_6,4,3 E4_6 F4_6,5,3 G1",       # Page number: 55
         '27b'    : "C4_b D4_6,4,3 E-4_6 F4_6,5,3 G1",    # Page number: 55
         '28'     : "G2_5,4 GG2_7 C1",                  # Page number: 55
